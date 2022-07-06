3c8aadcc91ba0212ed8c52877d9f1fc81da952a1
NguyenTienHCL <nguyentaitien2610@gmail.com>
2022-07-06 04:08:52 +0000

    poc

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-apt update -y
-apt install git -y
-apt install java-1.8.0-openjdk -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-apt install -y jenkins
-systemctl start jenkins
+sudo apt-get update -y
+#jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+
+#java
+sudo apt install openjdk-11-jre
+java -version
--- minikube-kubectl.sh
+++ minikube-kubectl.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install curl
-sudo apt-get install apt-transport-https
-sudo apt install virtualbox virtualbox-ext-pack
-sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo cp minikube-linux-amd64 /usr/local/bin/minikube
-sudo chmod 755 /usr/local/bin/minikube
-minikube version
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
